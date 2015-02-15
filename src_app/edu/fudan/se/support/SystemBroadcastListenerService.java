5fdf07e0fd4835c92af98654d98c3425b9c6d1db
JellyWhh <wanghuan19909@126.com>
2015-02-15 14:24:22 +0800

    完成了来电号码的设置以及测试，也就是指定来电号码会结束任务

--- src_app/edu/fudan/se/support/SystemBroadcastListenerService.java
+++ src_app/edu/fudan/se/support/SystemBroadcastListenerService.java
@@ -16,6 +16,8 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.os.IBinder;
+import android.telephony.PhoneStateListener;
+import android.telephony.TelephonyManager;
 
 /**
  * 监听各种系统广播的服务，在应用启动时启动服务
@@ -26,9 +28,11 @@ import android.os.IBinder;
 public class SystemBroadcastListenerService extends Service {
 
 	private BroadcastReceiver mReceiver;
+	private PhoneReceiver phoneReceiver;
 
 	public SystemBroadcastListenerService() {
 		mReceiver = new My_BroadcastReceiver();
+		phoneReceiver = new PhoneReceiver();
 	}
 
 	@Override
@@ -43,16 +47,23 @@ public class SystemBroadcastListenerService extends Service {
 				"-------SystemBroadcastListenerService onCreate()-------");
 		IntentFilter mFilter = new IntentFilter();
 		// mFilter.addAction("android.provider.Telephony.SMS_RECEIVED");// 新短信
-		mFilter.addAction(Intent.ACTION_SCREEN_OFF);// 屏幕关闭
+//		mFilter.addAction(Intent.ACTION_SCREEN_OFF);// 屏幕关闭
 		mFilter.addAction(Intent.ACTION_HEADSET_PLUG);// 耳机的插入和拔出
 		mFilter.addAction(Intent.ACTION_TIME_TICK); // 时间流逝
 		registerReceiver(mReceiver, mFilter);
+
+		// 电话
+		IntentFilter phoneFilter = new IntentFilter();
+		phoneFilter.addAction(Intent.ACTION_NEW_OUTGOING_CALL);
+		phoneFilter.addAction("android.intent.action.PHONE_STATE");
+		registerReceiver(phoneReceiver, phoneFilter);
 	}
 
 	@Override
 	public void onDestroy() {
 		super.onDestroy();
 		unregisterReceiver(mReceiver);
+		unregisterReceiver(phoneReceiver);
 	}
 
 	private class My_BroadcastReceiver extends BroadcastReceiver {
@@ -130,4 +141,69 @@ public class SystemBroadcastListenerService extends Service {
 		}
 	}
 
+	private class PhoneReceiver extends BroadcastReceiver {
+		
+
+		@Override
+		public void onReceive(Context context, Intent intent) {
+
+			android.util.Log.i("MY_LOG",
+					"-------SystemBroadcastListenerService PhoneReceiver intent action:"
+							+ intent.getAction());
+
+			if (intent.getAction().equals(Intent.ACTION_NEW_OUTGOING_CALL)) {
+				// 如果是去电（拨出）
+				android.util.Log
+						.i("MY_LOG",
+								"-------SystemBroadcastListenerService PhoneReceiver out phone");
+
+			} else {
+				// 查了下android文档，貌似没有专门用于接收来电的action,所以，非去电即来电
+				android.util.Log
+						.i("MY_LOG",
+								"-------SystemBroadcastListenerService PhoneReceiver in phone");
+				TelephonyManager tm = (TelephonyManager) context
+						.getSystemService(Service.TELEPHONY_SERVICE);
+				// 设置一个监听器
+				tm.listen(listener, PhoneStateListener.LISTEN_CALL_STATE);
+			}
+		}
+
+		PhoneStateListener listener = new PhoneStateListener() {
+
+			@Override
+			public void onCallStateChanged(int state, String incomingNumber) {
+				// TODO Auto-generated method stub
+				// state 当前状态 incomingNumber,貌似没有去电的API
+				super.onCallStateChanged(state, incomingNumber);
+				switch (state) {
+				case TelephonyManager.CALL_STATE_IDLE:
+					android.util.Log
+							.i("MY_LOG",
+									"-------SystemBroadcastListenerService PhoneReceiver phone idle");
+					break;
+				case TelephonyManager.CALL_STATE_OFFHOOK:
+					android.util.Log
+							.i("MY_LOG",
+									"-------SystemBroadcastListenerService PhoneReceiver phone offhook");
+					break;
+				case TelephonyManager.CALL_STATE_RINGING:
+					// 输出来电号码
+					android.util.Log
+							.i("MY_LOG",
+									"-------SystemBroadcastListenerService PhoneReceiver phone ringing, incomingNumber:"
+											+ incomingNumber);
+					//发送消息
+					SGMMessage msgToMessage = new SGMMessage(
+							MesHeader_Mes2Manger.LOCAL_AGENT_MESSAGE, null, null,
+							null, new MesBody_Mes2Manager("Phone"+incomingNumber));
+					GetAgent.getAideAgentInterface(
+							(SGMApplication) getApplication()).sendMesToManager(
+							msgToMessage);
+					break;
+				}
+			}
+		};
+
+	}
 }
